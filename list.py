a87db0cc5cb20410586309efb8a0dca9290f38ce
Sabarehmani <sabarehmani287@gmail.com>
2024-08-28 09:35:43 -0700

    lists

--- list.py
+++ list.py
@@ -3,4 +3,12 @@ print(marks)
 print(type(marks))
 print(marks[0])
 print(marks[1])
-print(len(marks))
+print(len(marks)) 
+
+student = ["Saba", 22, 23, "Rehmani"]
+print(student[0])
+student[0]= "Razzaq"
+print(student)
+
+marks = [45, 36, 12.3, 87, 42, 90]
+print(marks[1:5])
